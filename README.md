5d0a3be9505ec964207973134a7e9506d7d3aa78
kudaken <kudaken.sub@gmail.com>
2024-03-25 02:26:55 +0900

    2024/03/25/ upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# discord-embed-webhook
+誰でも簡単にdiscordのwebhookで埋め込みメッセージを送信できます。
+
+
+![image](https://i.imgur.com/RDGdYbI.png)
+
+↑こんな感じ
+
+## 使い方
+1. config.yamlに必要事項を入力してください。(スマホでwebhookを作る場合は[こちら](https://note.com/nuyaki/n/nc2f167ca6c5f)の記事をご参照ください。)
+2. main.pyを実行してください。
---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import yaml
+
+# YAMLファイルから設定を読み込む
+with open('config.yaml', 'r', encoding='utf-8') as config_file:
+    config_str = config_file.read()
+config = yaml.safe_load(config_str)
+
+# Discord Webhook URL
+WEBHOOK_URL = config['WebhookURL']
+
+def send_embedded_message_via_webhook(embed):
+    data = {
+        'embeds': [embed]
+    }
+    response = requests.post(WEBHOOK_URL, json=data)
+    if response.status_code == 204:
+        print("正常にメッセージが送信されました。")
+    else:
+        print("正常にメッセージが送信されませんでした。stats code:", response.status_code)
+
+# コマンドの説明
+embed = {
+    'title': config['title'],
+    'description': config['description'],
+    'color': 0XFFFFFF
+}
+
+# Embeddedメッセージを送信
+send_embedded_message_via_webhook(embed)
